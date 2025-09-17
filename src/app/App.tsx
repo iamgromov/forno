4e8b2851635a62d0add2572b4652c3f7d3a10bd0
iamgromov <iamgromov@gmail.com>
2025-09-17 17:58:14 +0300

    initial project

---
+++ src/app/App.tsx
@@ -0,0 +1,35 @@
+import Header from '../components/Header';
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import Card from '../components/Card';
+import data from '../assets/mock.json';
+import '../scss/app.scss';
+
+function App() {
+  return (
+    <div className='wrapper'>
+      <Header />
+      <div className='content'>
+        <div className='container'>
+          <div className='content__top'>
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className='content__title'>Все пиццы</h2>
+          <div className='content__items'>
+            {data.pizzas.map((el) => {
+              return (
+                <Card
+                  key={el.id}
+                  {...el}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Categories.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+function Categories() {
+  const [category, setCategory] = useState(0);
+
+  const categories = [
+    'Все',
+    'Мясные',
+    'Вегетарианская',
+    'Гриль',
+    'Острые',
+    'Закрытые',
+  ];
+
+  return (
+    <div className='categories'>
+      <ul>
+        {categories.map((elem, index) => {
+          return (
+            <li
+              key={elem}
+              onClick={() => setCategory(index)}
+              className={category === index ? 'active' : ''}
+            >
+              {elem}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
